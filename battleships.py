0835a97e5657d3be6a96b20ad94973b2346509f5
LiamEdwards931 <>
2023-08-10 09:36:24 +0000

    add guess validation to the column and row inputs

--- battleships.py
+++ battleships.py
@@ -34,4 +34,27 @@ def battleships():
             row_num += 1
         print('  ----------------')
 
+    # Main loop that runs the game
+    while any(SHIP in row for row in board):
+        print("\n Your Board:")
+        print_board(board)
+
+    # Players turn - prints column guess and row guess on two seperate lines for easier guessing in the terminal
+    column_guess = input("Guess a column (A-H): ").upper()
+    row_guess = int(input("Guess a Row (1-8): ")) -1
+
+    #Dictionary rows and columns
+    col_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+    # Guess validation for columns
+    if column_guess not in col_dict:
+            print("That is not a valid column. Try again\n")
+            continue
+    column_guess = col_dict[column_guess]
+    # Guess Validation for rows
+    if guess_row < 0 or guess_row >= BOARD_SIZE:
+            print("Invalid row. Try again.\n")
+            continue
+    
+
+    
 
